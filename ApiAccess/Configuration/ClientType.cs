ed82fd757243a72c484ab062a73437bc1ffeddff
Jonast96 <Jon.Astveit@hotmail.com>
2024-05-10 10:19:06 +0200

    example code

---
+++ ApiAccess/Configuration/ClientType.cs
@@ -0,0 +1,11 @@
+namespace HelseId.Samples.ApiAccess.Configuration;
+
+public enum ClientType
+{
+    UserLoginOnly,
+    ApiAccess,
+    ApiAccessWithRequestObject,
+    ApiAccessWithTokenExchange,
+    ApiAccessWithResourceIndicators,
+    ApiAccessForMultiTenantClient,
+}
---
+++ ApiAccess/Models/Organization.cs
@@ -0,0 +1,9 @@
+namespace HelseId.Samples.ApiAccess.Models;
+
+public enum OrganizationIds
+{
+    HansensLegekontor = 3,
+    FlaksvaagoeyKommune = 4,
+    FlaksvaagoeyKommuneBoOgOmsorgssenter = 5,    
+}
+
